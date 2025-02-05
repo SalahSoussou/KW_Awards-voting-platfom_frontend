2f2f74e59fbf1e4a2273e3ff322a8935a279d47a
amuybar <amuyunzubq@gmail.com>
2025-02-05 19:31:09 +0300

    Revert "Added A Navbar"
    This reverts commit 2043b7e28cb629b51b64b2f52974584c2b8815dd.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
-import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar/>
         <main className="p-8">
           <Routes>
-            {/* //Add Your Page Router HEre */}
+            //Add Your Page Router HEre
             <Route path="/register" element={<Register />} />
-            {/* When you Import your page you can Uncomment your route */}
-            {/* <Route path="/" element={<Home />} />
-             <Route path="/vote" element={<Vote />} />
-             <Route path="/blogs" element={<Blogs />} />
-             <Route path="/gallery" element={<Gallery />} />
-             <Route path="/aboutus" element={<AboutUs />} />
-             <Route path="/faqs" element={<FQAs />} /> */}
           </Routes>
         </main>
       </div>
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -3,9 +3,6 @@ const Register = () => {
       <div className="text-center">
         <h1 className="text-4xl font-bold">Register Page</h1>
         <p>Register to participate!</p>
-        <h1 className="text-3xl t font-bold underline">
-      Hello world!
-    </h1>
       </div>
     );
   };
